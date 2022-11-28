ead6b4ad50da1fe7b099c4a4382d45321b7566b9
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-28 16:30:31 -0500

    Add tests for the computer player

--- ship.test.js
+++ ship.test.js
@@ -158,6 +158,10 @@ test("Check check if all ships sunk on a gameboard - true", () => {
 });
 
 const playerOne = Player();
+const playerOneGB = Gameboard(); // Create empty gameboard for testing computer player
+
+const playerTwo = Player();
+playerTwo.computer = true;
 const playerTwoGB = Gameboard();
 const playerTwoShip = Ship(5);
 playerTwoGB.addShip(playerTwoShip, 0, 0, 'vertical');
@@ -168,4 +172,12 @@ test("Check if P1's attack on P2's gameboard works - hit", () => {
 
 test("Check if P1's attack on P2's gameboard works - miss", () => {
     expect(playerOne.attack(playerTwoGB, 0, 6)).toBe("miss");
+});
+
+test("Check if playerTwo is a computer", () => {
+    expect(playerTwo.computer).toBe(true);
+});
+
+test("Check if P2's attack on P1's gameboard works - miss", () => {
+    expect(playerTwo.attack(playerOneGB, 0, 0)).toBe("miss");
 });
