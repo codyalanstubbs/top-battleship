faa25e5232ce3c7794e5f6e840fe4d75f288d173
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-28 10:32:43 -0500

    Add function to set coordinates of a ship

--- ship.js
+++ ship.js
@@ -1,8 +1,8 @@
 export const Ship = (len) => {
     const length    = len;
     let hits        = 0;
-    const startCoord = [0,0];
-    const endCoord = [0,len-1];
+    let startCoord = [0,0];
+    let endCoord = [0,len-1];
 
     const getStartCoord = () => {return startCoord};
     const getEndCoord   = () => {return endCoord};
@@ -10,9 +10,22 @@ export const Ship = (len) => {
     const getHits   = () => {return hits};
     const hit       = () => {hits += 1};
     const isSunk    = () => {return hits === length};
+    const setCoord  = (startX, startY, axis) => {
+        startCoord[0]   = startX;
+        startCoord[1]   = startY;
+        if (axis === 'horizontal') {
+            endCoord[0]     = startX + length - 1;
+            endCoord[1]     = startY;
+        } else if ('vertical') {
+            endCoord[0]     = startX;
+            endCoord[1]     = startY + length - 1;
+        }
+
+        return [startCoord, endCoord];
+    }
 
     return {
         getStartCoord, getEndCoord, getLength, 
-        getHits, hit, isSunk
+        getHits, hit, isSunk, setCoord
     };
 }
