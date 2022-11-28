a3cf6a7cb552f2084d492eac6567cf6fcabd1bd0
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-28 10:06:32 -0500

    Add start and end coordinates and their getters

--- ship.js
+++ ship.js
@@ -1,11 +1,18 @@
 export const Ship = (len) => {
     const length    = len;
     let hits        = 0;
+    const startCoord = [0,0];
+    const endCoord = [0,len-1];
 
+    const getStartCoord = () => {return startCoord};
+    const getEndCoord   = () => {return endCoord};
     const getLength = () => {return length};
     const getHits   = () => {return hits};
     const hit       = () => {hits += 1};
     const isSunk    = () => {return hits === length};
 
-    return {getLength, getHits, hit, isSunk};
+    return {
+        getStartCoord, getEndCoord, getLength, 
+        getHits, hit, isSunk
+    };
 }
