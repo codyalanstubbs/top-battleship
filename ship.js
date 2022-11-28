608c15588588c3a5860166c641d9110832d33705
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-28 10:34:45 -0500

    Refactor for visually cleaner code

--- ship.js
+++ ship.js
@@ -1,15 +1,16 @@
 export const Ship = (len) => {
     const length    = len;
     let hits        = 0;
-    let startCoord = [0,0];
-    let endCoord = [0,len-1];
+    let startCoord  = [ 0,  0 ];
+    let endCoord    = [ 0,  len-1];
 
     const getStartCoord = () => {return startCoord};
     const getEndCoord   = () => {return endCoord};
-    const getLength = () => {return length};
-    const getHits   = () => {return hits};
-    const hit       = () => {hits += 1};
-    const isSunk    = () => {return hits === length};
+    const getLength     = () => {return length};
+    const getHits       = () => {return hits};
+    const hit           = () => {hits += 1};
+    const isSunk        = () => {return hits === length};
+
     const setCoord  = (startX, startY, axis) => {
         startCoord[0]   = startX;
         startCoord[1]   = startY;
