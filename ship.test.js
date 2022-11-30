d6539e841e04c4564babf8abc618bfbb38915288
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-30 10:18:14 -0500

    Create more descriptive statements for ship tests

--- ship.test.js
+++ ship.test.js
@@ -1,61 +1,68 @@
 import { Ship } from "./factories.js";
 
-const newShip = Ship(5);
+describe("Validate the Ship factory's getters", () => {
+    const newShip = Ship(5);
 
-test("ship length 5", () => {
-    expect(newShip.getLength()).toBe(5);
+    test("Should return ship length of 5", () => {
+        expect(newShip.getLength()).toBe(5);
+    });
+
+    test("Should return no hits, e.g.: 0", () => {
+        expect(newShip.getHits()).toBe(0);
+    });
+
+    test("Should return the default start coordinates [0, 0]", () => {
+        expect(newShip.getStartCoord()).toStrictEqual([0,0]);
+    });
+
+    test("Should return the default end coordinates [0, 5-1]", () => {
+        expect(newShip.getEndCoord()).toStrictEqual([0,4]);
+    });
 });
 
-test("No hits on ship", () => {
-    expect(newShip.getHits()).toBe(0);
+describe("Validate setting coordinates of horizontal ship", () => {
+    const setShipH = Ship(5);
+    setShipH.setCoord(1, 1, "horizontal");
+    
+    test("Should return the new start coordinates [1,1]", () => {
+        expect(setShipH.getStartCoord()).toStrictEqual([1,1]);
+    });
+    
+    test("Should return the new end coordinates [1+5-1, 1]", () => {
+        expect(setShipH.getEndCoord()).toStrictEqual([5,1]);
+    });
 });
 
-test("Get start coordinates", () => {
-    expect(newShip.getStartCoord()).toStrictEqual([0,0]);
+describe("Validate setting coordinates of vertical ship", () => {
+    const setShipV = Ship(5);
+    setShipV.setCoord(1, 1, "vertical");
+    
+    test("Should return the new start coordinates [1,1]", () => {
+        expect(setShipV.getStartCoord()).toStrictEqual([1,1]);
+    });
+    
+    test("Should return the new start coordinates [1, 1+5-1]", () => {
+        expect(setShipV.getEndCoord()).toStrictEqual([1,5]);
+    });
 });
 
-test("Get end coordinates", () => {
-    expect(newShip.getEndCoord()).toStrictEqual([0,4]);
-});
+describe("Validate the ships isSunk method", () => {
+    const ship = Ship(5);
 
-const setShipH = Ship(5);
-setShipH.setCoord(1, 1, "horizontal");
+    test("Should return 1 hit after 1 hit", () => {
+        ship.hit();
+        expect(ship.getHits()).toBe(1);
+    });
+    
+    test("Should return false after only 1 hit", () => {
+        expect(ship.isSunk()).toBe(false);
+    });
 
-test("Set new coordinates horizontally - check start", () => {
-    expect(setShipH.getStartCoord()).toStrictEqual([1,1]);
-});
-
-test("Set new coordinates horizontally - check end", () => {
-    expect(setShipH.getEndCoord()).toStrictEqual([5,1]);
-});
-
-const setShipV = Ship(5);
-setShipV.setCoord(1, 1, "vertical");
-
-test("Set new coordinates vertically - check start", () => {
-    expect(setShipV.getStartCoord()).toStrictEqual([1,1]);
-});
-
-test("Set new coordinates vertically - check end", () => {
-    expect(setShipV.getEndCoord()).toStrictEqual([1,5]);
-});
-
-const hitShip = Ship(5);
-hitShip.hit();
-test("Add hit to ship", () => {
-    expect(hitShip.getHits()).toBe(1);
-});
-
-test("Check if ship sunk after one hit", () => {
-    expect(hitShip.isSunk()).toBe(false);
-});
-
-const sunkShip = Ship(5);
-sunkShip.hit();
-sunkShip.hit();
-sunkShip.hit();
-sunkShip.hit();
-sunkShip.hit();
-test("Check if ship sunk after five hits", () => {
-    expect(sunkShip.isSunk()).toBe(true);
-});
+    test("Should return true after 4 more hits", () => {          
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+    });
+})
