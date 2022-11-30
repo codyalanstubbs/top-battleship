f89abf21519c2420ff27e64a5d2cd8525328dd3b
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-30 14:21:48 -0500

    Remove assets from src since static and put build js files in dist javascript directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        factories: './src/javascript/factories.js',
-        startBattle: './src/javascript/startBattle.js',
-        gameSetup: './src/javascript/gameSetup.js'
+        factories:  './src/javascript/factories.js',
+        startBattle:'./src/javascript/startBattle.js',
+        gameSetup:  './src/javascript/gameSetup.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist/javascript'),
     },
 };
